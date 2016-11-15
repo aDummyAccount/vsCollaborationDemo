f1592686e15665490ee08b8fc357dd66d1791f3e
TheCodeGnome <makmacdo@hotmail.com>
2016-11-14 19:08:54 -0500

    Initial commit

---
+++ vsCollaborationDemo/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(vsCollaborationDemo.Startup))]
+namespace vsCollaborationDemo
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
